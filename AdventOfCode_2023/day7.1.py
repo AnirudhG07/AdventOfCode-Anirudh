3b69edeee9df4d7893a9980f3372dbe69f8218fd
Anirudh Gupta <146579014+AnirudhG07@users.noreply.github.com>
2023-12-07 12:39:50 +0530

    Add files via upload

---
+++ AdventOfCode_2023/day7.1.py
@@ -0,0 +1,53 @@
+from functools import cmp_to_key 
+def frequency_string(hand):
+    freq = {}
+    for c in hand:
+        if c in freq:
+            freq[c] += 1
+        else:
+            freq[c] = 1
+    if len(freq)==1:
+        return 7
+    elif len(freq)==2:
+        if 1 in freq.values():
+            return 6
+        else:
+            return 5
+    elif len(freq)==3:
+        if 3 in freq.values():
+            return 4
+        else:
+            return 3
+    elif len(freq)==4:
+        return 2
+    else: 
+        return 1
+def comparison(hand1, hand2):
+    if frequency_string(hand1[0]) > frequency_string(hand2[0]):
+        return True
+    elif frequency_string(hand1[0]) == frequency_string(hand2[0]):
+        basis=['A','K','Q','J','T','9','8','7','6','5','4','3','2']
+        for i in range(5):
+            if basis.index(hand1[0][i]) < basis.index(hand2[0][i]):  # hand1 > hand2
+                return True
+            elif basis.index(hand1[0][i]) == basis.index(hand2[0][i]):
+                continue
+            else:
+                return False
+    else:
+        return False
+
+with open('Ques_Practise/AdventOfCode_2023/day7.txt') as f:
+    lines = f.readlines()
+    hands_bid=[]
+    for line in lines:
+        hands_bid.append(line.split())
+    # sort hands_bid[i][0] with comparison function
+    for i in range(len(hands_bid)):
+        for j in range(i+1, len(hands_bid)):
+            if comparison(hands_bid[i], hands_bid[j]):
+                hands_bid[i], hands_bid[j] = hands_bid[j], hands_bid[i]
+    winning_sum=0
+    for i in range(len(hands_bid)):
+        winning_sum+=(i+1)*int(hands_bid[i][1])
+    print(winning_sum)
